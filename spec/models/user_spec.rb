6a3649ee419be51bc520126b9824f5e8581d34e8
James Hwang <91juhwang@gmail.com>
2017-02-03 15:33:49 -0500

    Test new user signups and user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "from the User model" do
+	  context 'when a new user signs up' do 
+	  	let(:user) { FactoryGirl.build(:user) }
+
+      it 'should create a valid user' do 
+      	expect(user).to be_valid
+      end
+    end
+  end
 end
