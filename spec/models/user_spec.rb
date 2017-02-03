106f5b0e7438bd961d80498a3fc2766d00e70487
James Hwang <91juhwang@gmail.com>
2017-02-03 15:40:48 -0500

    Add failed case for user model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe User do
-	describe "from the User model" do
-	  context 'when a new user signs up' do 
-	  	let(:user) { FactoryGirl.build(:user) }
+  describe 'from the User model' do
+    context 'when a new user signs up' do
+      let(:user) { FactoryGirl.build(:user) }
 
-      it 'should create a valid user' do 
-      	expect(user).to be_valid
+      it 'should create a valid user' do
+        expect(user).to be_valid
+      end
+    end
+
+    context 'when email is not present' do
+      let(:user) { FactoryGirl.build(:user, email: ' ') }
+      
+      it 'should not create a user' do
+        expect(user).to_not be_valid
       end
     end
   end
