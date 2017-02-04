c47a8bdfd847f946757b6078492f3ff9b21905d4
91juhwang <91juhwang@gmail.com>
2017-02-04 14:31:04 -0500

    Add authentication method for users

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,10 @@ ActiveRecord::Schema.define(version: 20170203194438) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "auth_token",             default: ""
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,32 @@ describe User do
         expect(user).to_not be_valid
       end
     end
+
+    context 'when requests come in for the user' do
+
+      it 'should respond to the auth_token' do 
+        expect(User.new).to respond_to(:auth_token)
+      end
+    end
+    
+    context 'when #generate_authentication_token!' do 
+      let(:user) { FactoryGirl.build(:user) }
+
+      it 'should generate a valid authentication token' do
+        Devise.stub(:friendly_token).and_return("uniquetoken123")
+        user.generate_authentication_token!
+        expect(user.auth_token).to eql 'uniquetoken123'
+      end
+    end
+
+    context 'when the token is already taken from #generate_authentication_token! ' do 
+      let(:existing_user) { FactoryGirl.build(:user, auth_token: 'uniquetoken123') }
+      let(:user) { FactoryGirl.build(:user) }
+
+      it 'should generate another token' do
+        user.generate_authentication_token!
+        expect(user.auth_token).not_to eql existing_user.auth_token
+      end
+    end
   end
 end
